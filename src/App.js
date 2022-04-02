1163e005fa1bbe57edfce2f46f0dae7c006d645b
YaChugunov <informed.looser@gmail.com>
2022-04-02 14:13:25 +0300

    2 files modified

--- src/App.js
+++ src/App.js
@@ -2,18 +2,30 @@ import React from 'react';
 import './style.css';
 
 function Feedback() {
+  const [name, setName] = useState('');
+
+  const formHandler = (e) => {
+    e.preventDefault(); // При нажатии ничего не происходит
+    console.dir(e.target);
+  };
+
+  const inputHandler = (e) => {
+    e.preventDefault(); // При нажатии ничего не происходит
+    console.dir(e.target);
+  };
+
   return (
-  <form>
-    <label htmlFor="name">username</label>
-    <input type="text" name="name">
-  </form>
+    <>
+      <pre>{JSON.stringify(name)}</pre>
+      <form onSubmit={formHandler}>
+        <label htmlFor="name">username</label>
+        <input type="text" name="name" id="name" onChange={inputHandler} />
+        <button>Send</button>
+      </form>
+    </>
   );
 }
 
 export default function App() {
-  return (
-    <div>
-      <Feedback />
-    </div>
-  );
+  return <Feedback />;
 }
